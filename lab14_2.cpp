eedd65d03e65674106ed6a5eac8f05d4bb29d08e
apichaya <apichaya_ink@cmu.ac.th>
2023-01-13 16:40:26 +0700

    lab2 complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,39 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[N][M], int s , int x , int y)
+{
+    int i = 0 ,j = 0;
+    for (i = 0 ; i < 30 ; i++)
+    {
+        for(j = 0 ; j <70 ; j++)
+        {
+            if (sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1)
+            {
+                image[i][j] =1;
+            }
+        }
+    }
+    
+    
+}
+
+void showImage(const bool image[N][M])
+{
+    int i = 0 ,j = 0;
+    cout << "------------------------------------------------------------------------"<<endl;
+    for( i = 0 ; i<30 ; i++)
+    {
+       cout << "|";
+       for ( j = 0 ; j < 70 ; j++)
+       {
+         if (image[i][j] == 0) cout << " ";
+         if (image[i][j] == 1) cout << "*";
+       }
+       cout << "|";
+       cout << endl;
+    }
+    
+    cout << "------------------------------------------------------------------------"<<endl;
+}
